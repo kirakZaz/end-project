8bdb856b424da9e8fefbe65c3656cecb1c1914e4
zakirkaZ <kirka.zaz@gmail.com>
2021-12-10 19:13:13 +0200

    socket io

--- server.js
+++ server.js
@@ -3,8 +3,14 @@ const swaggerUi = require("swagger-ui-express");
 const express = require("express");
 
 require("dotenv").config({ path: ".env" });
+const hostname = process.env.DB_HOST;
+const port = process.env.PORT || 5000;
 
 const server = new express();
+
+server.set("port", port);
+server.set("view engine", "html");
+
 const http = require("http").createServer(server);
 const io = require("socket.io")(http);
 
@@ -14,9 +20,6 @@ const db = require("./models");
 const mongoRouter = require("./routes");
 
 const swaggerDocument = require("./config/swagger.json");
-
-const hostname = process.env.DB_HOST;
-const port = process.env.PORT || 5000;
 
 function main() {
   server.use(logger("dev"));
@@ -29,38 +32,9 @@ function main() {
     swaggerUi.setup(swaggerDocument, { explorer: true })
   );
 
-  server.set("view engine", "html");
-
   server.use(express.static(__dirname + "/views"));
 
   server.use("/api/", mongoRouter);
-
-  io.on("connection", function (socket) {
-    socket.on("stream", function (image) {
-      socket.broadcast.emit("stream", image);
-    });
-  });
-
-  io.on("connection", function (socket) {
-    socket.on("msg", function (data) {
-      //Send message to everyone
-      io.sockets.emit("newmsg", data);
-    });
-  });
-
-  server.get("/index.html", function (request, response) {
-    response.sendFile(__dirname + "/views/index.html");
-  });
-
-  server.get("/main", function (request, response) {
-    response.sendFile(__dirname + "/views/main.html");
-  });
-
-  server.get("/users", function (request, response) {
-    response.sendFile(__dirname + "/views/users.html");
-  });
-
-  server.get("/favicon.ico", (req, res) => res.status(204));
 
   db.mongoose
     .connect(db.url, {
@@ -77,6 +51,33 @@ function main() {
       console.log("Cannot connect to the database!", err);
       process.exit();
     });
+
+  server.get("/index.html", function (request, response) {
+    response.sendFile(__dirname + "/views/index.html");
+  });
+
+  server.get("/main", function (request, response) {
+    response.sendFile(__dirname + "/views/main.html");
+  });
+
+  server.get("/users", function (request, response) {
+    response.sendFile(__dirname + "/views/users.html");
+  });
+
+  server.get("/favicon.ico", (req, res) => res.status(204));
+
+  io.on("connection", function (socket) {
+    socket.on("stream", function (image) {
+      socket.broadcast.emit("stream", image);
+    });
+  });
+
+  io.on("connection", function (socket) {
+    socket.on("msg", function (data) {
+      //Send message to everyone
+      io.sockets.emit("newmsg", data);
+    });
+  });
 }
 
 main();
