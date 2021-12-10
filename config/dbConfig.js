0015dea9e0f62b37f1da6005c5999a033987fecf
zakirkaZ <kirka.zaz@gmail.com>
2021-12-10 18:02:13 +0200

    dev dep - to dep

--- config/dbConfig.js
+++
@@ -1,3 +0,0 @@
-export const dbConfig = {
-  url: "mongodb+srv://kiraZa:nvzhq0gy@cluster0.ejznt.mongodb.net/robo?retryWrites=true&w=majority",
-};
--- models/index.js
+++ models/index.js
@@ -1,11 +1,11 @@
-const { dbConfig } = require("../config/dbConfig");
+const uri = process.env.DATABASE_URL;
 
 const mongoose = require("mongoose");
 mongoose.Promise = global.Promise;
 
 const db = {};
 db.mongoose = mongoose;
-db.url = dbConfig.url;
+db.url = uri;
 
 db.user = require("./users");
 db.token = require("./tokens");
