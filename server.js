833554406a98b6fda994d424e12e0c369dad5393
zakirkaZ <kirka.zaz@gmail.com>
2021-12-10 20:28:26 +0200

    port

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ server.set("port", port);
 server.set("view engine", "html");
 
 const http = require("http").createServer(server);
-const io = require("socket.io")(http);
+const io = require("socket.io")(process.env.PORT, http);
 
 const logger = require("morgan");
 
@@ -67,18 +67,18 @@ function main() {
 
   server.get("/favicon.ico", (req, res) => res.status(204));
 
-  // io.on("connection", function (socket) {
-  //   socket.on("stream", function (image) {
-  //     socket.broadcast.emit("stream", image);
-  //   });
-  // });
-  //
-  // io.on("connection", function (socket) {
-  //   socket.on("msg", function (data) {
-  //     //Send message to everyone
-  //     io.sockets.emit("newmsg", data);
-  //   });
-  // });
+  io.on("connection", function (socket) {
+    socket.on("stream", function (image) {
+      socket.broadcast.emit("stream", image);
+    });
+  });
+
+  io.on("connection", function (socket) {
+    socket.on("msg", function (data) {
+      //Send message to everyone
+      io.sockets.emit("newmsg", data);
+    });
+  });
 }
 
 main();
