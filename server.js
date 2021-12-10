fae4d9310d9a2fca6c11e7f2c600b0c0c671c66d
zakirkaZ <kirka.zaz@gmail.com>
2021-12-10 18:53:57 +0200

    socket

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ require("dotenv").config({ path: ".env" });
 
 const app = express();
 const server = require("http").createServer(app);
-const io = require("socket.io").listen(server);
+const io = require("socket.io")(server);
 
 const logger = require("morgan");
 
